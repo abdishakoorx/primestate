b2494058d97b53645ef8c62b36a7c3dbe3ab4980
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-06 18:56:01 +0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-primary">
+      <h2>supp</h2>
+      <Button outline = 'outline' className="bg-secondary hover:bg-tertiary">Crapp</Button>
+    </div>
+  );
+}
