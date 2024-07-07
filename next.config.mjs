edc54e407f0a250dfa772defdcb614a109d9bfec
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-07 23:37:24 +0300

    adding domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['qorcyoboaerodoalqgco.supabase.co'],
+    },
+};
 
 export default nextConfig;
