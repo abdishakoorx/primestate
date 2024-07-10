fe994d544ad46fd7f47d265463782cce4db6fd2c
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-10 13:53:54 +0300

    created user listing

--- app/(routes)/user/[[...rest]]/page.jsx
+++ app/(routes)/user/[[...rest]]/page.jsx
@@ -1,10 +1,17 @@
-import { UserProfile } from '@clerk/nextjs'
+"use client"
+import { UserButton, UserProfile } from '@clerk/nextjs'
+import {  Castle } from 'lucide-react'
 import React from 'react'
+import UserListing from '../_components/UserListing'
 
 function User() {
   return (
     <div className='flex items-center justify-center bg-black'>
-        <UserProfile/>
+        <UserProfile>
+            <UserButton.UserProfilePage label='My Listing' labelIcon ={<Castle className='w-5 h-5'/>} url="my-listing" >
+                <UserListing />
+            </UserButton.UserProfilePage>
+        </UserProfile>
     </div>
   )
 }
