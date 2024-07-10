9a412d5bda5420b77add18b861347162d0cc7928
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-10 03:45:08 +0300

    created rent route

---
+++ app/(routes)/rent/page.jsx
@@ -0,0 +1,12 @@
+import ListingViewMap from '@/app/_components/ListingViewMap'
+import React from 'react'
+
+function ForRent() {
+  return (
+    <div>
+        <ListingViewMap type = 'Rent' />
+    </div>
+  )
+}
+
+export default ForRent
