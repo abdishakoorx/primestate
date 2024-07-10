6a024a5cd05f0ff083cf3fa00a43213e50e6d83e
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-10 12:08:00 +0300

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "#121212",
+        background: "#000",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "#E0E0E0",
