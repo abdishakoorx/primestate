c0804f8397c4b15a7993d94107d012cbcac1924e
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-11 20:04:46 +0300

    changing home route

--- middleware.js
+++ middleware.js
@@ -1,23 +1,7 @@
-import { NextResponse } from 'next/server';
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/home"],
-  afterAuth(auth, req, evt) {
-    const url = new URL(req.url);
-    
-    // Redirect all requests to root URL to /home
-    if (url.pathname === '/') {
-      return NextResponse.redirect(new URL('/home', req.url));
-    }
-
-    // Handle users who aren't authenticated trying to access protected routes
-    if (!auth.userId && !auth.isPublicRoute) {
-      return NextResponse.redirect(new URL('/home', req.url));
-    }
-  },
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
