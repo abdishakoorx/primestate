f9e03d88360f4b0de0bd869a7161bd2713ebb345
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-11 20:38:23 +0300

    added responsive menu

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,7 +1,8 @@
 "use client"
+import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { SignOutButton, UserButton, useUser } from '@clerk/nextjs';
-import { Plus } from 'lucide-react'
+import { Plus, Menu, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -18,61 +19,76 @@ import {
 function Header() {
   const { user, isSignedIn } = useUser()
   const pathname = usePathname();
-
-  const ProtectedLink = ({ href, children }) => {
-    return isSignedIn ? (
-      <Link href={href}>{children}</Link>
-    ) : (
-      <Link href="/sign-in">{children}</Link>
-    );
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
   };
 
+
+  const NavItem = ({ href, children }) => (
+    <Link href={href} onClick={handleLinkClick}>
+      <li className={`font-medium cursor-pointer hover:text-tertiary ${pathname === href ? 'text-tertiary' : ''}`}>
+        {children}
+      </li>
+    </Link>
+  );
+
   return (
-    <div className='fixed top-0 z-10 flex justify-between w-full p-6 px-10 bg-black shadow-sm shadow-quantenary'>
-      <div className='flex items-center gap-20'>
-        <div className="flex items-center justify-center">
-          <Link href={'/'} >
-            <Image src='/logo.png' alt='logo' width={120} height={120} 
-            />
+    <div className='fixed top-0 z-10 w-full bg-black shadow-sm shadow-quantenary'>
+      <div className='flex items-center justify-between p-6 md:px-10'>
+        <div className="flex items-center gap-20">
+          <Link href={'/'}>
+            <Image src='/logo.png' alt='logo' width={120} height={120} />
           </Link>
+          
+          <ul className='hidden gap-10 md:flex'>
+            <NavItem href="/">Home</NavItem>
+            <NavItem href="/buy">Buy Property</NavItem>
+            <NavItem href="/rent">Rent Property</NavItem>
+            <NavItem href="/agent-finder">Agent Finder</NavItem>
+          </ul>
         </div>
         
-        <ul className='hidden gap-10 md:flex'>
-          <Link href={'/'} className={`font-medium cursor-pointer hover:text-tertiary ${pathname === '/' ? 'text-tertiary' : ''}`}>
-            Home
+        <div className='flex items-center gap-2 md:gap-6'>
+          <Link href={isSignedIn ? '/add_new_listing' : 'sign-in'}>
+            <Button className='bg-opacity-90 bg-tertiary hover:bg-opacity-100 hover:bg-tertiary'>
+              <Plus className='w-5 h-5' />Post
+            </Button>
           </Link>
-          <Link href={'/buy'}>
-            <li className={`font-medium cursor-pointer hover:text-tertiary ${pathname === '/buy' ? 'text-tertiary' : ''}`}>
-              Buy Property
-            </li>
-          </Link>
-          <Link href={'/rent'}>
-            <li className={`font-medium cursor-pointer hover:text-tertiary ${pathname === '/rent' ? 'text-tertiary' : ''}`}>
-              Rent Property
-            </li>
-          </Link>
-          <Link href={'/agent-finder'}>
-            <li className={`font-medium cursor-pointer hover:text-tertiary ${pathname === '/agent-finder' ? 'text-tertiary' : ''}`}>
-              Agent Finder
-            </li>
-          </Link>
-        </ul>
+          
+          {isSignedIn ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Image src={user?.imageUrl} width={40} height={40} alt='user' className='rounded-full cursor-pointer' />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className='cursor-pointer'><Link href={'/user'}>My Listing</Link></DropdownMenuItem>
+                <DropdownMenuItem className='cursor-pointer'><SignOutButton>Logout</SignOutButton></DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Link href={'/sign-in'}><Button className='bg-secondary hover:bg-tertiary'>Sign in</Button></Link>
+          )}
+          
+          <button className="md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
       </div>
-      <div className='flex items-center gap-2 md:gap-6'>
-        <Link href={isSignedIn ? '/add_new_listing' : 'sign-in'}><Button className='bg-opacity-90 bg-tertiary hover:bg-opacity-100 hover:bg-tertiary'><Plus className='w-5 h-5 ' />Post</Button></Link>
-        {isSignedIn ?
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild><Image src={user?.imageUrl} width={40} height={40} alt='user' className='rounded-full cursor-pointer' /></DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem className='cursor-pointer'><Link href={'/user'}>My Listing</Link></DropdownMenuItem>
-              <DropdownMenuItem className='cursor-pointer'> <SignOutButton>Logout</SignOutButton></DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          :
-          <Link href={'/sign-in'}><Button className='bg-secondary hover:bg-tertiary'>Sign in</Button></Link>}
-      </div>
+      
+      {/* Mobile menu */}
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <ul className="flex flex-col items-center py-4">
+            <NavItem href="/">Home</NavItem>
+            <NavItem href="/buy">Buy Property</NavItem>
+            <NavItem href="/rent">Rent Property</NavItem>
+            <NavItem href="/agent-finder">Agent Finder</NavItem>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
