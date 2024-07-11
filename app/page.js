0626e5c51774f81ca84fbb71aaeea9a8380c91cf
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-11 20:09:30 +0300

    new landing page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import ListingViewMap from "./_components/ListingViewMap";
+import FooterSection from "./_components/FooterSection";
+import HeroSection from "./_components/HeroSection";
+import HomeSection from "./_components/HomeSection";
+import OwnerSection from "./_components/OwnerSection";
+import Testemonial from "./_components/Testemonial";
 
 export default function Home() {
   return (
     <div>
-      
+      <HeroSection/>
+      <HomeSection />
+      <OwnerSection />
+      <Testemonial />
+      <FooterSection />
     </div>
   );
 }
